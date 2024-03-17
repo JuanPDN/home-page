01c2f9094206589e05cd0bbbcbf40fa9708e3b78
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-03-16 19:57:37 -0500

    aside menu

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const theme = document.getElementById("toogle");
+const theme = document.getElementById("toogle");
 
-  theme.addEventListener("change", function () {
-    const htmlElement = document.documentElement;
-    const isChecked = theme.checked;
-    const logo = document.getElementById("logo");
-    const imageTheme = document.getElementById("imageTheme");
+theme.addEventListener("change", function () {
+  const htmlElement = document.documentElement;
+  const isChecked = theme.checked;
+  const logo = document.getElementById("logo");
+  const imageTheme = document.getElementById("imageTheme");
 
-    if (!isChecked) {
-      htmlElement.classList.remove("dark");
-      htmlElement.classList.add("light");
-      logo.src = "/public/assets/alarado-icon-homepage.svg";
-      imageTheme.src = "/public/assets/sun_fill.svg";
-    } else {
-      htmlElement.classList.remove("light");
-      htmlElement.classList.add("dark");
-      logo.src = "/public/assets/alarado-icon-homepage-light.svg";
-      imageTheme.src = "/public/assets/Moon_fill.svg";
-    }
-  });
-  
+  if (!isChecked) {
+    htmlElement.classList.remove("dark");
+    htmlElement.classList.add("light");
+    logo.src = "/public/assets/alarado-icon-homepage.svg";
+    imageTheme.src = "/public/assets/sun_fill.svg";
+  } else {
+    htmlElement.classList.remove("light");
+    htmlElement.classList.add("dark");
+    logo.src = "/public/assets/alarado-icon-homepage-light.svg";
+    imageTheme.src = "/public/assets/Moon_fill.svg";
+  }
 });
+
+const open = document.getElementById('open')
+const main = document.getElementById('main')
+const close = document.getElementById('close')
+
+open.addEventListener('click', function(){
+    main.classList.remove('invisible')
+})
+
+close.addEventListener('click', function(){
+    main.classList.add('invisible')
+})
