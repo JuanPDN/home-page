f8a1f97a2ba0bda904c5a48388ec751464497fd8
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-03-18 09:01:15 -0500

    fix bug of theme

--- src/main.js
+++ src/main.js
@@ -1,36 +1,40 @@
-
 //cambiar tema
-const theme = document.getElementById("toogle");
+const theme = document.querySelectorAll("#toogle, #toogle2");
 
-theme.addEventListener("change", function () {
-  const htmlElement = document.documentElement;
-  const isChecked = theme.checked;
-  const logo = document.getElementById("logo");
-  const imageTheme = document.getElementById("imageTheme");
+theme.forEach((element) => {
+  element.addEventListener("change", function () {
+    const htmlElement = document.documentElement;
+    const isChecked = element.checked;
+    const logo = document.querySelector(".logo");
+    const imageTheme = document.querySelectorAll("#imageTheme, #imageTheme2");
 
-  if (!isChecked) {
-    htmlElement.classList.remove("dark");
-    htmlElement.classList.add("light");
-    logo.src = "/public/assets/alarado-icon-homepage.svg";
-    imageTheme.src = "/public/assets/sun_fill.svg";
-  } else {
-    htmlElement.classList.remove("light");
-    htmlElement.classList.add("dark");
-    logo.src = "/public/assets/alarado-icon-homepage-light.svg";
-    imageTheme.src = "/public/assets/Moon_fill.svg";
-  }
+    if (!isChecked) {
+      htmlElement.classList.remove("dark");
+      htmlElement.classList.add("light");
+      logo.src = "/public/assets/alarado-icon-homepage.svg";
+      imageTheme.forEach((element) => {
+        element.src = "/public/assets/Sun_fill.svg";
+      });
+    } else {
+      htmlElement.classList.remove("light");
+      htmlElement.classList.add("dark");
+      logo.src = "/public/assets/alarado-icon-homepage-light.svg";
+      imageTheme.forEach((element) => {
+        element.src = "/public/assets/Moon_fill.svg";
+      });
+    }
+  });
 });
 
-
 //Cerrar y abbrir menu
-const open = document.getElementById('open');
-const main = document.getElementById('main');
-const close = document.getElementById('close');
+const open = document.getElementById("open");
+const main = document.getElementById("main");
+const close = document.getElementById("close");
 
-open.addEventListener('click', function(){
-    main.classList.remove('invisible');
-})
+open.addEventListener("click", function () {
+  main.classList.remove("invisible");
+});
 
-close.addEventListener('click', function(){
-    main.classList.add('invisible');
-})
+close.addEventListener("click", function () {
+  main.classList.add("invisible");
+});
