070da9b92a1726af40a4462b616455c2f40e7950
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-03-19 19:14:49 -0500

    fixed slide bar

--- src/main.js
+++ src/main.js
@@ -1,54 +1,42 @@
 //cambiar tema
-const theme = document.querySelectorAll("#toogle, #toogle2");
+const theme = document.querySelector("#toogle");
 
-theme.forEach((element) => {
-  element.addEventListener("change", function () {
-    const htmlElement = document.documentElement;
-    const isChecked = element.checked;
-    const logo = document.querySelector(".logo");
-    const imageTheme = document.querySelectorAll("#imageTheme, #imageTheme2");
+theme.addEventListener("change", function () {
+  const htmlElement = document.documentElement;
+  const isChecked = theme.checked;
+  const logo = document.querySelector(".logo");
+  const imageTheme = document.querySelector("#imageTheme");
+  const menu = document.querySelector(".ImageMenu");
 
-    if (!isChecked) {
-      htmlElement.classList.remove("dark");
-      htmlElement.classList.add("light");
-      logo.src = "./assets/alarado-icon-homepage.svg";
-      imageTheme.forEach((element) => {
-        element.src = "./assets/Sun_fill.svg";
-      });
-      element.checked = false;
-    } else {
-      htmlElement.classList.remove("light");
-      htmlElement.classList.add("dark");
-      logo.src = "./assets/alarado-icon-homepage-light.svg";
-      imageTheme.forEach((element) => {
-        element.src = "./assets/Moon_fill.svg";
-      });
-      element.checked = true;
-    }
-  });
+  if (!isChecked) {
+    htmlElement.classList.remove("dark");
+    htmlElement.classList.add("light");
+    logo.src = "./assets/alarado-icon-homepage.svg";
+    imageTheme.src = "./assets/Sun_fill.svg";
+  } else {
+    htmlElement.classList.remove("light");
+    htmlElement.classList.add("dark");
+    logo.src = "./assets/alarado-icon-homepage-light.svg";
+    imageTheme.src = "./assets/Moon_fill.svg";
+  }
 });
 
-//Cerrar y abbrir menu
-// const open = document.getElementById("open");
-// const main = document.getElementById("main");
-// const close = document.getElementById("close");
-
-// open.addEventListener("click", function () {
-//   main.classList.remove("hidden");
-// });
-
-// close.addEventListener("click", function () {
-//   main.classList.add("hidden");
-// });
-
 const menu = document.getElementById("main");
-const imageMenu = document.querySelector(".imageMenu");
+const close_menu = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
+stroke-width="1.5" stroke="black" class="w-6 h-6">
+<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+</svg>`;
+const open_menu = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
+stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+</svg>`;
 
 menu.addEventListener("change", function () {
+  const imageMenu = document.querySelector(".imageMenu");
   const isChecked = menu.checked;
   if (isChecked) {
-    imageMenu.src = "/src/assets/close_menu.svg";
+    imageMenu.innerHTML = close_menu;
   } else {
-    imageMenu.src = "/src/assets/menu.svg";
+    imageMenu.innerHTML = open_menu;
   }
 });
